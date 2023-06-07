cdbbda4bec456923bb5021d84936a47f4979be9b
Yordano21 <134991759+Yordano21@users.noreply.github.com>
2023-06-06 21:41:04 -0500

    Update script.js
    modificado

--- script.js
+++ script.js
@@ -2,29 +2,30 @@ const textArea = document.querySelector(".text-area");
 const mensaje = document.querySelector(".mensaje");
 const copia = document.querySelector(".copiar");
 
-function botonEncriptar(){
-    const textoEncriptado = encriptar(textArea.value)
-    mensaje.value = textoEncriptado
-    textArea.value = "";
-    mensaje.style.backgroundImage = "none";
+function btnEncriptar(){
+    const textoEncriptado = textArea.value;
+    if (/^[a-z]+$/.test(textoEncriptado)) {
+        const textoEncriptadoModificado = encriptar(textoEncriptado);
+        mensaje.value = textoEncriptadoModificado;
+        textArea.value = "";
+        mensaje.style.backgroundImage = "none";
+    } else {
+        alert("Ingrese solo letras minúsculas y sin acentos");
+    }
 }
 
 
 
 
 function encriptar(stringEncriptada){
-
-    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
-    stringEncriptada = stringEncriptada.toLowerCase()
-
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o","ober"], ["u","ufat"]];
+    stringEncriptada = stringEncriptada.toLowerCase();
     for(let i = 0; i < matrizCodigo.length; i++){
-
         if(stringEncriptada.includes(matrizCodigo[i][0])){
-            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
-
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo [i][1]);
         }
     }
-    return stringEncriptada
+    return stringEncriptada;
 }
 
 function botonDesencriptar(){
